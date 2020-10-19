c326fff8a4cdcf918c956066f89a27389539810d
sreejesh <sreejesh@kriyatma.com>
2020-10-19 18:27:37 +0530

    initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Secure-AWS-S3-bucket-access-via-cloudfront-signedurl-Example
-Secure AWS S3 bucket access via cloudfront signedurl Example
+This example shows how to generate a cloudfront signed url with an expiry time for s3 bucket access. 
---
+++ index.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('./pk.pem', {encoding: 'utf8'});
+
+const Signer = new AWS.CloudFront.Signer(process.env.PUBLIC_KEY, privateKey) ;
+const getSignedUrl = async (event) => {
+  console.log(event);
+    const mediaUrl = event.headers["object-key"];
+    if(!mediaUrl){
+      return "key is missing in header";
+    }
+    const cloudUrl = process.env.CLOUD_URL;
+    const signingParams = {
+        url: `https://${cloudUrl}.cloudfront.net/${mediaUrl}`, 
+        expires: Math.round(new Date().getTime()/1000) + 1200
+      }
+      
+      
+      return new Promise((resolve, reject) => {
+            Signer.getSignedUrl(signingParams, (err, signedUrl) => {
+                  console.log(err, signedUrl)
+                  if(signedUrl){
+                    return resolve({signedUrl})
+                  }
+                  return reject(err)
+                })
+          })
+}
+exports.handler = getSignedUrl;
+
+
+
